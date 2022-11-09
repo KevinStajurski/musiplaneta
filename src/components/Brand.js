4dac8b6e82f7a444a3427ba975b85477f9353452
Kevin Stajurski <kevinstajurski@gmail.com>
2022-11-09 11:00:55 -0300

    PreEntrega1+Stajurski

---
+++ src/components/Brand.js
@@ -0,0 +1,9 @@
+import '../App.css'
+import Logo from '../assets/musiplaneta.png'
+const Brand = () => {
+    return(
+            <img src={Logo} alt="logo de la tienda" className='Logo'/>
+    )
+}
+
+export default Brand
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import '../App.css'
 import Categories from './Categories'
 import CartWidget from './CartWidget'
+import Brand from './Brand'
 const NavBar = () => {
     return(
         <div className='NavBar'>
+            <Brand/>
             <Categories/>
             <CartWidget/>
         </div>
