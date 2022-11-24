46634ab2619a792bceb5ae599adc356ee8bcc495
Kevin Stajurski <kevinstajurski@gmail.com>
2022-11-23 21:25:50 -0300

    link a home desde brand

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,8 +1,12 @@
 import '../App.css'
 import Logo from '../assets/musiplaneta.png'
+import {Link} from "react-router-dom"
 const Brand = () => {
     return(
+        <Link to="/">
             <img src={Logo} alt="logo de la tienda" className='Logo'/>
+        </Link>
+            
     )
 }
 
