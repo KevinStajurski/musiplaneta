3a7e0413ae0b407147a32dcd6ca0c9f10bbec18e
Kevin Stajurski <kevinstajurski@gmail.com>
2022-11-30 18:25:09 -0300

    se agrego boton de detalles a los items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom"
 const Item = ({product}) => {
     return(
         <div className="divProd">
             <img className="imgProd" src={product.img} alt=""/>
             <li className="li">'{product.marca} {product.modelo}'</li>
+            <button><Link to={`/item/${product.id}`} className="links">Detalles</Link></button>
         </div>
     )
 }
