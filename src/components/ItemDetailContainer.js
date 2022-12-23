cdc5d47f45f0e03836d62ad25754808075a13d82
Kevin Stajurski <kevinstajurski@gmail.com>
2022-12-22 23:11:56 -0300

    firebase en itemdetail

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,17 +1,21 @@
 import {useParams} from "react-router-dom"
 import { useEffect, useState } from "react";
-import {getProducts} from "../data/products"
 import ItemDetail from "./ItemDetail"
+import {doc, getDoc, getFirestore} from "firebase/firestore"
 
 const ItemDetailContainer = () => {
     const {id} = useParams()
     const [producto, setProducto] = useState([]);
-    
-    useEffect(() => {
-        getProducts().then((productos) => {
-        setProducto(productos[id]);
-        });
-    }, [id])
+
+    useEffect(()=>{
+        const db = getFirestore()
+        const itemRef = doc(db, "items", id)
+        getDoc(itemRef).then((snapshot)=>{
+            if (snapshot.exists()) {
+                setProducto(snapshot.data())
+            }
+        })
+    },[id])
 
     return <ItemDetail producto={producto}/>
 }
