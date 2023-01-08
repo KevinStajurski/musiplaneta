cb9206125976e3435ab6a5c25d37cd9159dc2108
Kevin Stajurski <kevinstajurski@gmail.com>
2023-01-08 16:29:18 -0300

    se corrigio count.js

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,11 +1,5 @@
-import {useState} from "react"
 import {ItemCount} from './ItemCount'
-const ItemDetail = ({producto}) => {
-    const [count, setCount] = useState(1)
-    const [currentStock, setCurrentStock] = useState(producto.stock)
-    const maxQuantity = currentStock
-    console.log(currentStock)
-
+const ItemDetail = ({producto, count, setCount, currentStock, setCurrentStock, maxQuantity}) => {
     function handleCount(type) {
         if (type === "mas" && count < maxQuantity) setCount(count + 1)
         if (type === "menos" && count > 1) setCount(count - 1)
@@ -22,7 +16,7 @@ const ItemDetail = ({producto}) => {
             <h1 className="nombreProducto">{producto.marca} {producto.modelo}</h1>
             <h2>$ {producto.precio}</h2>
             <h3>Stock: {producto.stock}</h3>
-            <span>{producto.descripcion}</span>
+            <p>{producto.descripcion}</p>
             <ItemCount count={count} handleCount={handleCount} handleAdd={handleAdd}/>
         </div>
     )
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -6,6 +6,9 @@ import {doc, getDoc, getFirestore} from "firebase/firestore"
 const ItemDetailContainer = () => {
     const {id} = useParams()
     const [producto, setProducto] = useState([]);
+    const [count, setCount] = useState(1)
+    const [currentStock, setCurrentStock] = useState(0)
+    const [maxQuantity, setMaxQuantity] = useState(0)
 
     useEffect(()=>{
         const db = getFirestore()
@@ -13,11 +16,13 @@ const ItemDetailContainer = () => {
         getDoc(itemRef).then((snapshot)=>{
             if (snapshot.exists()) {
                 setProducto({id, ...snapshot.data()})
+                setCurrentStock(producto.stock)
+                setMaxQuantity(producto.stock)
             }
         })
-    },[])
+    },[id, producto.stock])
 
-    return <ItemDetail producto={producto}/>
+    return <ItemDetail producto={producto} count={count} setCount={setCount} currentStock={currentStock} setCurrentStock={setCurrentStock} maxQuantity={maxQuantity}/>
 }
 
 export default ItemDetailContainer
